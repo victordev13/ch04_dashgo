f937a67a3f207279831c12f52b50374696ddd9a4
victordev13 <vtrcarvalho.13@gmail.com>
2021-11-08 21:20:06 -0300

    feat: SideBar

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,9 +1,14 @@
+import { Flex } from '@chakra-ui/react';
 import { Header } from '../components/Header/Header';
+import { Sidebar } from '../components/Sidebar/Sidebar';
 
 export default function DashBoard() {
   return (
-    <div>
+    <Flex direction="column" h="100vh">
       <Header />
-    </div>
+      <Flex w="100%" my="6" maxWidth={1400} mx="auto"px="6">
+        <Sidebar/>
+      </Flex>
+    </Flex>
   );
 }
