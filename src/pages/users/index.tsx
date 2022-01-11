d20b6ece91c167e3d84b47b5214304efd64708ab
victordev13 <vtrcarvalho.13@gmail.com>
2022-01-11 19:45:46 -0300

    feat: hook useUsers

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -21,41 +21,17 @@ import { Header } from '../../components/Header';
 import { Pagination } from '../../components/Pagination';
 import { Sidebar } from '../../components/Sidebar';
 import Link from 'next/link';
-import { useQuery } from 'react-query';
-import { api } from '../../services/api';
+import { useUsers } from '../../hooks/useUsers';
 
 export default function UserList() {
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
+    RiAddLine,
   });
 
-  const { data, isLoading, isFetching, error } = useQuery(
-    'users',
-    async () => {
-      const { data } = await api.get('/users');
-      const users = data.users.map((user) => {
-        return {
-          id: user.id,
-          name: user.name,
-          email: user.email,
-          createdAt: new Date(
-            user.createdAt
-          ).toLocaleDateString('pt-BR', {
-            day: '2-digit',
-            month: 'long',
-            year: 'numeric',
-          }),
-        };
-      });
-
-      return users;
-    },
-    {
-      staleTime: 1000 * 5, // durante 5 segundos não revalida os dados
-    }
-  );
-
+  const { data, isLoading, isFetching, error } = useUsers();
+  console.log(data);
   return (
     <Box>
       <Header />
